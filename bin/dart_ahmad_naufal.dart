811e4f5ffbe1c4865196c680d33ba0ee8074d63d
unknown <madnaufal12@gmail.com>
2023-10-03 12:49:00 +0700

    Change

--- bin/dart_ahmad_naufal.dart
+++ bin/dart_ahmad_naufal.dart
@@ -1,5 +1,5 @@
 import 'package:dart_ahmad_naufal/dart_ahmad_naufal.dart' as dart_ahmad_naufal;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_ahmad_naufal.calculate()}!');
+  print('Hello world');
 }
